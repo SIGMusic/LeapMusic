0345cd3a52eae5e30752d07638b63d408a84041e
lesniak2 <tomlesniak@hotmail.com>
2014-02-16 13:10:45 -0600

    Added basic receive and print functionality

---
+++ InterpreterLayer/AudioLayer.py
@@ -0,0 +1,54 @@
+#################
+#   IMPORTS	#
+#################
+import sys
+from simpleOSC import initOSCServer, startOSCServer, closeOSC, setOSCHandler
+import OSC
+
+import time
+
+#################
+#   VARIABLES   #
+#################
+PRINT = 1 # 0/1, print messages when recieved
+ip = "127.0.0.1" # receiving osc from 
+port = 9000 # main port
+#################
+#      MAIN     #
+#################
+def run():
+	
+	# OSC SERVER/HANDLERS #
+	initOSCServer(ip, port) # setup OSC server
+        setOSCHandler('/print', printing_handler) # adding our print function
+
+	# SERVER START #
+	startOSCServer()
+	print "server is running, listening at " + str(ip) + ":" + str(port)
+        print "use Ctrl-C to quit"
+
+	# SERVER LOOP#
+	try:
+		clock = 1
+		while(1):
+			time.sleep(5)
+	except KeyboardInterrupt:
+		print "closing all OSC connections... and exit"
+		closeOSC() # close the osc connection before exiting	
+
+
+#####################
+#   OSC HANDLERS    #
+#####################
+
+# define a message-handler function for the server to call.
+def printing_handler(addr, tags, stuff, source):
+    if(PRINT):
+        print "---"
+        print "received new osc msg from %s" % OSC.getUrlStr(source)
+        print "with addr : %s" % addr
+        print "typetags %s" % tags
+        print "data %s" % stuff
+        print "---"
+
+run()
