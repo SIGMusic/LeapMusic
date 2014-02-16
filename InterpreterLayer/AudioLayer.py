bd886a6983da7f71ed8d011245b18f0e9132ee41
lesniak2 <tomlesniak@hotmail.com>
2014-02-16 15:08:40 -0600

    Cleaned up code, deleted print handler

--- InterpreterLayer/AudioLayer.py
+++ InterpreterLayer/AudioLayer.py
@@ -18,41 +18,32 @@ port = 9433
 #      MAIN     #
 #################
 def run():
-	
-	# OSC SERVER/HANDLERS #
-	initOSCServer(ip, port) # setup OSC server
-        setOSCHandler('/print', printing_handler) # adding our print function
-        setOSCHandler('/shape', shape_handler)
-        setOSCHandler('/boundary', boundary_handler)
-	# SERVER START #
-	startOSCServer()
-	print "server is running, listening at " + str(ip) + ":" + str(port)
-        print "use Ctrl-C to quit"
 
-	# SERVER LOOP#
-	try:
-		clock = 1
-		while(1):
-			time.sleep(5)
-	except KeyboardInterrupt:
-		print "closing all OSC connections... and exit"
-		closeOSC() # close the osc connection before exiting	
+    # OSC SERVER/HANDLERS #
+    initOSCServer(ip, port) # setup OSC server
+	
+    setOSCHandler('/shape', shape_handler)
+    setOSCHandler('/boundary', boundary_handler)
+    # SERVER START #
+    startOSCServer()
+    print "server is running, listening at " + str(ip) + ":" + str(port)
+    print "use Ctrl-C to quit"
+
+    # SERVER LOOP#
+    try:
+        clock = 1
+        while(1):
+            time.sleep(5)
+    except KeyboardInterrupt:
+        print "closing all OSC connections... and exit"
+        closeOSC() # close the osc connection before exiting	
 
 
 #####################
 #   OSC HANDLERS    #
 #####################
 
-# define a message-handler function for the server to call
-def printing_handler(addr, tags, stuff, source):
-    if(PRINT):
-        print "---"
-        print "received new osc msg from %s" % OSC.getUrlStr(source)
-        print "with addr : %s" % addr
-        print "typetags %s" % tags
-        print "data %s" % stuff
-        print "---"
-        
+# define message-handler functions for the server to call        
 def shape_handler(addr, tags, stuff, source) :
     if(PRINT_SHAPE):
         print "---"
@@ -61,7 +52,7 @@ def shape_handler(addr, tags, stuff, source) :
         print "typetags %s" % tags
         print "data %s" % stuff
         print "---"
-        time.sleep(.5)
+        time.sleep(.5) # make packets readable
 
 def boundary_handler(addr, tags, stuff, source) :
     if(PRINT_BOUNDARY) :
@@ -71,5 +62,5 @@ def boundary_handler(addr, tags, stuff, source) :
         print "typetags %s" % tags
         print "data %s" % stuff
         print "---"
-        time.sleep(.5)
+        time.sleep(.5) # make packets readable
 run()
