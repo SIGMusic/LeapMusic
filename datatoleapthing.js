dcf61a1510b34fdd85fd3e9c12e547858fae3fd3
Elvin <edtubillara@gmail.com>
2014-02-23 12:56:07 -0600

    added js message interface

---
+++ datatoleapthing.js
@@ -0,0 +1,9 @@
+{
+	"Synchronous (once per frame)" : {
+		"/event" : [name (String)],
+	},
+	"Asynchronous (on event)" : {
+		"/percussion" : [instrument (String)], //drums and other percussion instruments
+		"/minstruments" : [instrument1 (String), midival (int), velocity(int), other(int)], //melodic instruments
+	}
+}
