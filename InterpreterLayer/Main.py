39a073a5297eb2bc2dfb29d054aad2119569e997
Elvin <edtubillara@gmail.com>
2014-03-09 13:01:07 -0500

    added synths and midi

--- InterpreterLayer/Main.py
+++ InterpreterLayer/Main.py
@@ -19,108 +19,59 @@ class MusicGenerator:
            Initialize the music generator class
         """
         self.ip = "127.0.0.1"
-        self.pdportnumber = 9003 # where to send data
+        self.pdportnumber = 9033 # where to send data
         self.bars_to_send = Queue.Queue()
         self.total_bars = Queue.Queue()
         initOSCClient(self.ip, self.pdportnumber)
         self.total_calls = 0
-        self.midi_file_name = "mary.mid"
+        self.midi_file_name = "guile__s_theme.mid"
         self.resolution = 0
-        self.note_buffer_list = NoteBufferList(total_bars=self.total_bars, resolution=self.resolution)
+        self.song_buffer = None
+        self.ms_per_tick = 0  # milliseconds per tick
 
-    def flush(self):
-        value = 0
-        for index in range(0,63):
-            sendOSCMsg("/sync/mellosynth_buffer/m1/midivalue/index", [index])
-            sendOSCMsg("/sync/mellosynth_buffer/m1/midivalue/value", [value])
-            sendOSCMsg("/sync/mellosynth_buffer/m2/midivalue/index", [index])
-            sendOSCMsg("/sync/mellosynth_buffer/m2/midivalue/value", [value])
-
-    def send_bar(self, index, value, synth_number, start):
+    def send_event(self, arg, channel_number):
         """
-        sends a midi value
+        sends a midi event
         """
-        address = "m1"
-        if synth_number == 1:
-            sendOSCMsg("/sync/mellosynth_buffer/m1/midivalue/index", [index])
-            sendOSCMsg("/sync/mellosynth_buffer/m1/midivalue/value", [value])
-            if start:
-                sendOSCMsg("/sync/mellosynth_buffer/m1/startnote/index", [index])
-                sendOSCMsg("/sync/mellosynth_buffer/m1/startnote/value", [1])
-            else:
-                sendOSCMsg("/sync/mellosynth_buffer/m1/startnote/index", [index])
-                sendOSCMsg("/sync/mellosynth_buffer/m1/startnote/value", [0])
-        else:
-            sendOSCMsg("/sync/mellosynth_buffer/m2/midivalue/index", [index])
-            sendOSCMsg("/sync/mellosynth_buffer/m2/midivalue/value", [value])
-            if start:
-                sendOSCMsg("/sync/mellosynth_buffer/m2/startnote/index", [index])
-                sendOSCMsg("/sync/mellosynth_buffer/m2/startnote/value", [1])
-            else:
-                sendOSCMsg("/sync/mellosynth_buffer/m2/startnote/index", [index])
-                sendOSCMsg("/sync/mellosynth_buffer/m2/startnote/value", [0])
 
-        print "sent osc to pd", self.total_calls
-        self.total_calls+=1
+        (index, midi_val, velocity, tick) = arg
+        routing_message = "/sync/c" + str(channel_number) + "/midi"
+        sendOSCMsg(routing_message, [midi_val, velocity, tick, index])
 
     def buffer_handler(self, addr, tags, stuff, source):
-        self.pop_current_queue()
+        self.send_buffer()
         print "buffer was switched"
 
     def send_start_signal(self):
         """
         Sends the start signal to the pure data patch
         """
+        sendOSCMsg("/ms_per_tick", [self.ms_per_tick])
         sendOSCMsg("/start", [1])
-        print "sent start signal"
 
-    def pop_current_queue(self):
-        """
-            sends pure data the current queue
-            the current buffer will be replaced
-        """
-        print "sending next buffer..."
-        while not (Queue.Queue.empty(self.bars_to_send)):
-            item = Queue.Queue.get(self.bars_to_send)
-            print "item is", item
-            (index, value, synth_number, start) = item
-            self.send_bar(index, value, synth_number,start)
+        print "sent start signal with metro,lopass", self.ms_per_tick, 700
 
-        #TODO rename these dumb variables
-        if self.total_bars.empty():
-            return
-
-        (index,dum,dum2,dum3) = Queue.Queue.get(self.total_bars)
-        Queue.Queue.put(self.bars_to_send, (index,dum,dum2,dum3))
-        i = index
-        while i < index+index+31:
-            if(self.total_bars.empty()):
-                break
-            (i,dum,dum2,dum3) = Queue.Queue.get(self.total_bars)
-            Queue.Queue.put(self.bars_to_send, (i,dum,dum2,dum3))
-
-
-    def put_dummy_queue(self):
-        """
-            This puts dummy values in the queue
-        """
-        for i in range(0, 256):
-            Queue.Queue.put(self.total_bars, (i % 64, (i + 40) % 90))
+    def send_buffer(self):
+        print "sending buffer",
+        channels = self.song_buffer.get_channel_events()
+        channel_number = 0
+        for channel in channels:
+            channel_number += 1
+            instrument_id_message = "/sync/c" + str(channel_number) + "/i_number/"
+            instrument_parameter_message = "/sync/c" + str(channel_number) + "/p_instrument/"
+            volume_instrument_message = "/sync/c" + str(channel_number) + "/v_instrument/"
+            sendOSCMsg(instrument_id_message, [1])
+            sendOSCMsg(instrument_parameter_message, [.4])
+            sendOSCMsg(volume_instrument_message, [.5])
+            for event in channel:
+                self.send_event(event, channel_number)
+        print "sent:", channel_number
 
     def start(self):
-        buffer_switcher = BufferSwitcherServer(self.buffer_handler)
-        buffer_switcher_thread = threading.Thread(None, buffer_switcher.start)
-        buffer_switcher_thread.start()
-        (index,dum,dum2,dum3) = Queue.Queue.get(self.total_bars)
-        Queue.Queue.put(self.bars_to_send, (index,dum,dum2,dum3))
-        i = index
-        while i < index+index+63:
-            if(self.total_bars.empty()):
-                break
-            (i,dum,dum2,dum3) = Queue.Queue.get(self.total_bars)
-            Queue.Queue.put(self.bars_to_send, (i,dum,dum2,dum3))
-
-        self.pop_current_queue()
+       # buffer_switcher = BufferSwitcherServer(self.buffer_handler)
+       # buffer_switcher_thread = threading.Thread(None, buffer_switcher.start)
+       # buffer_switcher_thread.start()
+        self.send_buffer()
         self.send_start_signal()
 
         try:
@@ -131,60 +82,81 @@ class MusicGenerator:
             sendOSCMsg("/start", [1])
             closeOSC()  # close the osc connection before exiting
 
+    def hard_reset_buffer(self):
+        for channel_number in range(1,17):
+            routing_message = "/sync/c" + str(channel_number) + "/midi/clean"
+            sendOSCMsg(routing_message,[1])
+            print "clearing channel:", channel_number, routing_message
+
     def load_midi_file(self, file_location):
         midi_file = midi.read_midifile(file_location)
-        i = 0
-        track = midi_file.pop()
+        midi_file.reverse()
+        midi_file.make_ticks_abs()
+        #print midi_file
         self.resolution = midi_file.resolution
-        self.note_buffer_list.set_resolution(self.resolution)
+        song_info_track = midi_file.pop()
 
-        for thing in track:
-            if type(thing) is midi.NoteOnEvent:
-                tick = thing.tick
-                i = self.note_buffer_list.add(midi_val=thing.data[0], tick=tick, velocity=thing.data[1], i=i)
-                print "added note"
-            if type(thing) is midi.NoteOffEvent:
-                print thing
+        has_tempo_event = False
+        mpqn = 0
+
+        for event in song_info_track:
+
+            if type(event) is midi.SetTempoEvent:
+                print event
+                has_tempo_event = True
+                mpqn = (event.data[0] << 16) | (event.data[1] << 8) | event.data[2]
+                print "mpqn is&: ", mpqn
+
+        if mpqn==0:
+            mpqn = 1
+        current_tempo_in_beats_per_minute = 120
+
+        if has_tempo_event:
+            current_tempo_in_beats_per_minute = 60000000.0/mpqn
+
+        self.ms_per_tick = self.resolution * (current_tempo_in_beats_per_minute / 60.0)
+        self.ms_per_tick = 1000.0/self.ms_per_tick
+        self.song_buffer = SongBuffer(time_per_tick=self.ms_per_tick)
+        print self.ms_per_tick
+
+        for track in midi_file:
+            self.song_buffer.add_track(track)
+
+        # self.song_buffer.display()
 
 
-class NoteBufferList:
-    def __init__(self, total_bars, resolution):
-        self.resolution = 0
-        self.synths = [3,2,1]
-        self.synths_being_used = {}
-        self.total_bars = total_bars
-        self.current_notes_to_add = {}
+class SongBuffer:
+    def __init__(self, time_per_tick):
+        self.time_per_tick = time_per_tick
+        self.channels = []
+        self.channel_count = 0
 
+    def add_track(self, track):
+        channel_events = []
+        add_index = 0
+        for event in track:
+            if type(event) is midi.NoteOnEvent or type(event) is midi.NoteOffEvent:
+                self._add_to_channel_events(channel_events, event.data[0], event.data[1], event.tick, add_index)
+                add_index += 1
 
-    def set_resolution(self,resolution):
-        self.resolution = resolution
+        self.channels.append(channel_events)
+        if channel_events:
+            self.channel_count += 1
 
-    def add(self, midi_val, tick, velocity, i):
+    def _add_to_channel_events(self, event, midi_val, velocity, tick, add_index):
+        event.append((add_index, midi_val, velocity, tick))
+        return add_index
 
-        if midi_val in self.current_notes_to_add.keys():
-            if velocity == 0:
-                del self.current_notes_to_add[midi_val]
-                synth = self.synths_being_used[midi_val]
-                del self.synths_being_used[midi_val]
-                self.synths.append(synth)
+    def get_channel_events(self):
+        return self.channels
 
-            for thing in self.current_notes_to_add.values():
-                (index_number, curr_midi_value, synth_number, start) = thing
-                Queue.Queue.put(self.total_bars, (index_number, curr_midi_value, synth_number, 0))
-
-        else:
-            synth = self.synths.pop()
-            self.synths_being_used[midi_val]=synth
-            self.current_notes_to_add[midi_val]=(i, midi_val, synth,1)
-            for thing in self.current_notes_to_add.values():
-                (index_number, curr_midi_value, synth_number, start) = thing
-                if synth_number != synth:
-                    Queue.Queue.put(self.total_bars, (index_number, curr_midi_value, synth_number, 0))
-                else:
-                    Queue.Queue.put(self.total_bars, (index_number, curr_midi_value, synth_number, 1))
-                    print "inserting index, value, synth",thing
-
-        return i + tick // (self.resolution/2)
+    def display(self):
+        print "channel/track count:", self.channel_count
+        channel = 1
+        for channel_events in self.channels:
+            for event in channel_events:
+                print "(channel,event): (", channel, ",", event, ")"
+        channel += 1
 
 
 class BufferSwitcherServer:
@@ -206,7 +178,8 @@ class BufferSwitcherServer:
 if __name__ == "__main__":
 
     mg = MusicGenerator()
-    mg.flush()
+    # mg.hard_reset_buffer()
+  #  time.sleep(5)
     mg.load_midi_file(mg.midi_file_name)
-    mgThread = threading.Thread(None,mg.start)
+    mgThread = threading.Thread(None, mg.start)
     mgThread.start()
